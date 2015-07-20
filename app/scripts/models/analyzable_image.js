a96665a777e82841ab91bb51afd23968aafd1154
joe-re <joe.tialtngo@gmail.com>
2015-07-20 23:57:06 +0900

    refactor for readable

--- app/scripts/models/analyzable_image.js
+++ app/scripts/models/analyzable_image.js
@@ -38,28 +38,27 @@ class AnalyzableImage {
 
   getFilteredImage(pos) {
     const color = this.getColor(pos.x, pos.y);
-    let image = this.getImage();
+    const image = this.getImage();
+    const canvas = this.createCanvas(image);
 
-    let canvas = this.createCanvas(image);
     let context = canvas.getContext('2d');
     context.drawImage(image, 0, 0);
     const src = context.getImageData(0, 0, canvas.width, canvas.height);
     let dst = context.createImageData(canvas.width, canvas.height);
-    let i = 0;
-    let whiteCount = 0;
-    let blackCount = 0;
-    while(i < src.data.length) {
+
+    for (let i = 0; i < src.data.length; i += 4) {
       const compared = new Color(src.data[i], src.data[i + 1], src.data[i + 2]);
       const fillColor = color.isSimilar(compared) ? Color.BLACK : Color.WHITE;
       dst.data[i]     = fillColor.r;
       dst.data[i + 1] = fillColor.g;
       dst.data[i + 2] = fillColor.b;
       dst.data[i + 3] = src.data[i + 3]; // Alpha Property
-      i += 4;
     }
+
     context.putImageData(dst, 0, 0);
-    image.src = canvas.toDataURL();
-    return image;
+    const newImage = new Image();
+    newImage.src = canvas.toDataURL();
+    return newImage;
   }
 
   analyzeOcr(pos) {
